a7dfce05aa6d56e8607e680522480e751c61ac85
undefined <79967015+hwm-jmz@users.noreply.github.com>
2022-06-17 16:47:03 +0800

    Update circular-progress-bar.vue

--- docs/examples/progress/circular-progress-bar.vue
+++ docs/examples/progress/circular-progress-bar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="demo-progress">
     <el-progress type="circle" :percentage="0" />
-    <el-progress type="circle" :percentage="25" />
+    <el-progress type="circle" :percentage="65" />
     <el-progress type="circle" :percentage="100" status="success" />
     <el-progress type="circle" :percentage="70" status="warning" />
     <el-progress type="circle" :percentage="50" status="exception" />
